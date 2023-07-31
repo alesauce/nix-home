35f12e8e0a97a193c2dfe64b4a43b32ca7f5c20c
Alexander Sauceda <alexander@alexandersauceda.dev>
2023-07-30 18:59:45 -0700

    Modifying macOS dock behavior

--- nix-darwin/base/default.nix
+++ nix-darwin/base/default.nix
@@ -42,12 +42,21 @@
         dock = {
           autohide = true;
           mru-spaces = false;
+          orientation = "right";
           show-recents = false;
+          # Uncomment to show only open applications on the dock
+          # static-only = true;
         };
         finder.CreateDesktop = false;
         magicmouse.MouseButtonMode = "TwoButton";
         menuExtraClock.Show24Hour = true;
+        NSGlobalDomain = {
+          "com.apple.keyboard.fnState" = true;
+          "com.apple.swipescrolldirection" = false;
+        };
         screencapture.location = "/Users/alesauce/screenshots";
+        SoftwareUpdate.AutomaticallyInstallMacOSUpdates = true;
+        spaces.spans-displays = false;
       };
 
       # TODO: look into additional keymappings for base computer or disabling keyboard:

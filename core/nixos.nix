5e4f7d7cf7d6253a4d7ea73bb8727525c55be22c
Alexander Sauceda <alexander@alexandersauceda.dev>
2024-07-02 11:17:25 -0600

    init nixos core module

---
+++ core/nixos.nix
@@ -0,0 +1,80 @@
+{
+  config,
+  lib,
+  pkgs,
+  home-manager,
+  lanzaboote,
+  nix-index-database,
+  stylix,
+  ...
+}: {
+  imports = [
+    home-manager.nixosModules.home-manager
+    lanzaboote.nixosModules.lanzaboote
+    nix-index-database.nixosModules.nix-index
+    stylix.nixosModules.stylix
+    ./tmux.nix
+  ];
+
+  boot.kernelParams = ["log_buf_len=10M"];
+
+  documentation = {
+    dev.enable = true;
+    man.generateCaches = true;
+  };
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  networking = {
+    firewall = {
+      checkReversePath = "loose";
+      trustedInterfaces = ["tailscale0"];
+      allowedUDPPorts = [config.services.tailscale.port];
+    };
+    useDHCP = false;
+    useNetworkd = true;
+    wireguard.enable = true;
+  };
+
+  programs = {
+    command-not-found.enable = false;
+    mosh.enable = true;
+    zsh.enableGlobalCompInit = false;
+  };
+
+  security = {
+    sudo = {
+      enable = true;
+      wheelNeedsPassword = lib.mkDefault false;
+    };
+  };
+
+  services = {
+    dbus.implementation = "broker";
+    openssh = {
+      enable = true;
+      settings.PermitRootLogin = lib.mkDefault "no";
+    };
+    tailscale.enable = true;
+    fwupd.daemonSettings.EspLocation = config.boot.loader.efi.efiSysMountPoint;
+  };
+
+  system = {
+    extraSystemBuilderCmds = ''
+      ln -sv ${pkgs.path} $out/nixpkgs
+    '';
+
+    stateVersion = "23.05";
+  };
+
+  systemd = {
+    enableUnifiedCgroupHierarchy = true;
+    network.wait-online.anyInterface = true;
+    services.tailscaled = {
+      after = ["network-online.target" "systemd-resolved.service"];
+      wants = ["network-online.target" "systemd-resolved.service"];
+    };
+  };
+
+  users.mutableUsers = false;
+}

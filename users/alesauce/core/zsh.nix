7f530177140ff738cbfc326fe8dedb6dae6d7246
Alexander Sauceda <alexander@alexandersauceda.dev>
2024-08-02 22:08:41 -0600

    Removing unused ZSH config

--- users/alesauce/core/zsh.nix
+++ users/alesauce/core/zsh.nix
@@ -31,16 +31,6 @@
 
       source ${pkgs.zsh-autopair.src}/zsh-autopair.plugin.zsh
 
-      ${pkgs.nix-your-shell}/bin/nix-your-shell zsh | source /dev/stdin
-      bindkey "''${terminfo[khome]}" beginning-of-line
-      bindkey "''${terminfo[kend]}" end-of-line
-      bindkey "''${terminfo[kdch1]}" delete-char
-      bindkey "^[[1;5C" forward-word
-      bindkey "^[[1;3C" forward-word
-      bindkey "^[[1;5D" backward-word
-      bindkey "^[[1;3D" backward-word
-      bindkey -s "^O" 'fzf | xargs -r $EDITOR^M'
-
       NEW_USER="''${(C)USERNAME}"
       if [ -e ~/$NEW_USER-config ]; then
         source ~/$NEW_USER-config/entry-point

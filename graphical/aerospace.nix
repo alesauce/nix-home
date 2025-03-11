e094a16387ee2cee1471acc941d480fe10fe5112
Alexander Sauceda <alesauce@amazon.com>
2025-03-10 18:00:49 -0600

    refactor(graphical/aerospace): Use dynamic attributes for hyper and meh modifiers

--- graphical/aerospace.nix
+++ graphical/aerospace.nix
@@ -1,4 +1,7 @@
-{
+let
+  hyper = "cmd-alt-ctrl-shift";
+  meh = "ctrl-shift-alt";
+in {
   services.aerospace = {
     enable = true;
     settings = {
@@ -40,23 +43,23 @@
           binding = {
             alt-y = "layout tiles horizontal vertical";
             alt-t = "layout accordion horizontal vertical";
-            cmd-alt-ctrl-shift-l = "focus right";
-            cmd-alt-ctrl-shift-k = "focus up";
-            cmd-alt-ctrl-shift-j = "focus down";
-            cmd-alt-ctrl-shift-h = "focus left";
+            "${hyper}-l" = "focus right";
+            "${hyper}-k" = "focus up";
+            "${hyper}-j" = "focus down";
+            "${hyper}-h" = "focus left";
             ctrl-f1 = "workspace 1";
             ctrl-f2 = "workspace 2";
             ctrl-f3 = "workspace 3";
             ctrl-f4 = "workspace 4";
             ctrl-f5 = "workspace 5";
             ctrl-f6 = "workspace 6";
-            ctrl-shift-alt-1 = "move-node-to-workspace 1";
-            ctrl-shift-alt-2 = "move-node-to-workspace 2";
-            ctrl-shift-alt-3 = "move-node-to-workspace 3";
-            ctrl-shift-alt-4 = "move-node-to-workspace 4";
-            ctrl-shift-alt-5 = "move-node-to-workspace 5";
-            ctrl-shift-alt-6 = "move-node-to-workspace 6";
-            ctrl-shift-alt-s = "mode service";
+            "${meh}-1" = "move-node-to-workspace 1";
+            "${meh}-2" = "move-node-to-workspace 2";
+            "${meh}-3" = "move-node-to-workspace 3";
+            "${meh}-4" = "move-node-to-workspace 4";
+            "${meh}-5" = "move-node-to-workspace 5";
+            "${meh}-6" = "move-node-to-workspace 6";
+            "${meh}-s" = "mode service";
             alt-tab = "workspace-back-and-forth";
           };
         };

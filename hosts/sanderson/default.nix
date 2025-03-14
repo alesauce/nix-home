29e4e4c363c785660ac1c58ee10903afecaf909e
Alexander Sauceda <alexander@alexandersauceda.dev>
2025-03-14 13:07:07 -0600

    feat(hosts/sanderson): Add stylix config and enforce wheel needs password for sudo

--- hosts/sanderson/default.nix
+++ hosts/sanderson/default.nix
@@ -4,7 +4,19 @@
     ../../graphical
     ../../users/alesauce
   ];
+
   networking = {
     hostName = "sanderson";
+  };
+
+  security = {
+    sudo.wheelNeedsPassword = true;
+  };
+
+  stylix.fonts.sizes = {
+    desktop = 16;
+    applications = 14;
+    terminal = 12;
+    popups = 16;
   };
 }

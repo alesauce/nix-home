a01423e458028f5330ef696d8512e9a4b5a92524
Alexander Sauceda <alexander@alexandersauceda.dev>
2025-08-28 22:37:11 -0600

    feat(graphical/nixos): Enable COSMIC desktop

--- graphical/nixos.nix
+++ graphical/nixos.nix
@@ -1,1 +1,13 @@
-{}
+{
+  services = {
+    displayManager = {
+      cosmic-greeter.enable = true;
+    };
+    desktopManager = {
+      cosmic = {
+        enable = true;
+        xwayland.enable = true;
+      };
+    };
+  };
+}

173fe87393e8a300ea6390dde8e6862d7b253d1b
Alexander Sauceda <alexander@alexandersauceda.dev>
2026-04-17 16:39:34 -0600

    feat: Add pre-commit dev shell hook

--- modules/flake-parts/dev-shells.nix
+++ modules/flake-parts/dev-shells.nix
@@ -7,10 +7,9 @@
     devShells.default = pkgs.mkShell {
       name = "nix-home";
 
-      # TODO: uncomment after setting up pre-commit hooks
-      #shellHook = ''
-      #  ${config.pre-commit.installationScript}
-      #'';
+      shellHook = ''
+        ${config.pre-commit.installationScript}
+      '';
     };
   };
 }

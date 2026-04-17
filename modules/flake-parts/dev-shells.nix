5724cfbb8db7d5863884b29b11e6c0e60f2e34ee
Alexander Sauceda <alexander@alexandersauceda.dev>
2026-04-17 16:41:18 -0600

    fix: Add pre-commit enabled packages to dev-shell inputs

--- modules/flake-parts/dev-shells.nix
+++ modules/flake-parts/dev-shells.nix
@@ -7,6 +7,8 @@
     devShells.default = pkgs.mkShell {
       name = "nix-home";
 
+      nativeBuildInputs = config.pre-commit.settings.enabledPackages;
+
       shellHook = ''
         ${config.pre-commit.installationScript}
       '';

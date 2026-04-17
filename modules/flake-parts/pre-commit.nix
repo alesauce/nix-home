591e5521382cd27dcbd5aad0a3a9b8bba2f0e986
Alexander Sauceda <alexander@alexandersauceda.dev>
2026-04-17 16:38:36 -0600

    feat: Add pre-commit hooks

---
+++ modules/flake-parts/pre-commit.nix
@@ -0,0 +1,25 @@
+{inputs, ...}: {
+  imports = [inputs.git-hooks.flakeModule];
+  perSystem = {
+    pre-commit = {
+      check.enable = true;
+      settings.hooks = {
+        actionlint.enable = true;
+        check-added-large-files.enable = true;
+        check-merge-conflicts.enable = true;
+        deadnix = {
+          enable = true;
+          settings = {
+            noLambdaArg = true;
+            noLambdaPatternNames = true;
+            noUnderscore = true;
+          };
+        };
+        nil.enable = true;
+        shellcheck.enable = true;
+        statix.enable = true;
+        treefmt.enable = true;
+      };
+    };
+  };
+}

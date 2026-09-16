0f7e16dae3e133689178598d6d673d6536e79972
Alexander Sauceda <alexander@alexandersauceda.dev>
2026-09-15 22:33:32 -0600

    feat(ci): Set up full neovim build only if files are changed

--- wrappedPrograms/neovim/default.nix
+++ wrappedPrograms/neovim/default.nix
@@ -6,6 +6,19 @@
   neovimModule = {
     imports = [(inputs.import-tree ./_config)];
   };
+
+  allLanguagesModule = _: {
+    config.languages = {
+      bash.enable = true;
+      haskell.enable = true;
+      java.enable = true;
+      kotlin.enable = true;
+      nix.enable = true;
+      rust.enable = true;
+      scala.enable = true;
+      typescript.enable = true;
+    };
+  };
 in {
   flake.modules.programs.neovim = neovimModule;
 
@@ -17,6 +30,14 @@ in {
     packages.neovim = inputs.nixvim.legacyPackages.${system}.makeNixvimWithModule {
       inherit pkgs;
       module = neovimModule;
+      extraSpecialArgs.theme = config.flake.meta.theme;
+    };
+
+    packages.neovim-full = inputs.nixvim.legacyPackages.${system}.makeNixvimWithModule {
+      inherit pkgs;
+      module = {
+        imports = [neovimModule allLanguagesModule];
+      };
       extraSpecialArgs.theme = config.flake.meta.theme;
     };
   };
